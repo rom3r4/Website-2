a1bb787804709f96f5d3b011eb7933c2abe532d1
James Brooks <james@bluebaytravel.co.uk>
2015-02-07 14:58:31 +0000

    Update the footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -118,13 +118,30 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2 centered">
-                    <h5>support@cachethq.io</h5>
-                    <p class="mt">
+                <div class="col-xs-12 col-sm-4">
+                    <h4>About</h4>
+                    <p>Developed by <a href="https://james-brooks.uk">James Brooks</a> and a whole bunch of awesome <a href="https://github.com/cachethq/Cachet/graphs/contributors">contributors</a>.</p>
+                </div>
+                <div class="col-xs-12 col-sm-4">
+                    <h4>Social Links</h4>
+                    <p>
                         <a href="https://twitter.com/cachethq" target="_blank"><i class="ion-social-twitter"></i></a>
                         <a href="https://facebook.com/cachethq" target="_blank"><i class="ion-social-facebook"></i></a>
                         <a href="https://github.com/cachethq" target="_blank"><i class="ion-social-github"></i></a>
                     </p>
+                </div>
+                <div class="col-xs-12 col-sm-4">
+                    <h4>Newsletter</h4>
+                    <form class='form-vertical' role='form' action="//cachethq.us9.list-manage.com/subscribe/post?u=654f358550ef074d6601475cd&amp;id=711cf8b66d" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+                        <div class="form-group">
+                            <label for="mce-EMAIL" class='sr-only'>Email Address  <span class="asterisk">*</span></label>
+                            <input type="email" value="" required name="EMAIL" class="form-control subscribe-input" id="mce-EMAIL">
+                        </div>
+                        <div class='form-group'>
+                            <button type="submit" class='btn btn-green subscribe-submit' name="subscribe" id="mc-embedded-subscribe">Sign up</button>
+                        </div>
+                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_654f358550ef074d6601475cd_711cf8b66d" tabindex="-1" value=""></div>
+                    </form>
                 </div>
             </div>
         </div>
