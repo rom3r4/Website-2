ffb31392d15686a3aa8d799897ae36ea4a5f4ca8
James Brooks <james@bluebaytravel.co.uk>
2015-02-07 11:59:57 +0000

    Working on new website design.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/img/favicon.ico">
+
+    <title>CachetHQ</title>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+              <img class='img-responsive' src='/img/logo.png' width='175'>
+          </a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Cachet keeps your <strong>customers</strong>, <strong>developers</strong>, <strong>share-holders</strong> up to date with your products status.</h1>
+                    <button class="btn btn-conf btn-green">Learn More</button>
+                    <button class="btn btn-conf btn-clear"><i class='ion-social-github'></i> Download from GitHub</button>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="green">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4><i class="ion-social-github"></i> Open Source</h4>
+                    <p>Cachet is 100% open source, opening a whole world of contributions and new features.</p>
+                </div>
+                <div class="col-md-4">
+                    <h4><i class="ion-heart"></i> Beautiful</h4>
+                    <p>From the status page itself, to the dashboard. Cachet is beautifulyl crafted.</p>
+                </div>
+                <div class="col-md-4">
+                    <h4><i class="ion-planet"></i> Multilingual</h4>
+                    <p>Cachet speaks several languages <strong>and</strong> you can contrinute to existing translations or provide new ones from within Cachet itself.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="grey side-image text-heavy clearfix">
+        <div class="image-container col-md-5 col-sm-3 pull-left">
+            <div class="background-image-holder">
+                <img class="background-image" alt="Background Image" src="/img/side.jpg">
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4 content clearfix">
+                    <h1>Who are we?</h1>
+                    <p class="lead">Cachet is built by a team of three core developers, but with contributions from some amazing people!</p>
+                    <div class="col-sm-4 no-pad-left feature">
+                        <h5>James Brooks</h5>
+                        <p>Some info about James.</p>
+                    </div>
+                    <div class="col-sm-4 no-pad-left feature">
+                        <h5>Joe Cohen</h5>
+                        <p>Some info about Joe.</p>
+                    </div>
+                    <div class="col-sm-4 no-pad-left feature">
+                        <h5>Graham Campbell</h5>
+                        <p>Some info about Graham.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="row mtb">
+            <div class="col-md-12">
+                <img class="img-responsive aligncenter" src="/img/main-interface.jpg">
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 centered">
+                    <h5>support@cachethq.io</h5>
+                    <p class="mt">
+                        <a href="https://twitter.com/cachethq" target="_blank"><i class="ion-social-twitter"></i></a>
+                        <a href="https://facebook.com/cachethq" target="_blank"><i class="ion-social-facebook"></i></a>
+                        <a href="https://github.com/cachethq" target="_blank"><i class="ion-social-github"></i></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/retina-1.1.0.js"></script>
+  </body>
+</html>
