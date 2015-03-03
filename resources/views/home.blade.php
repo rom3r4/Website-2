f944afd0bd9663c7a9ac55c8ede64d45da9efd9a
James Brooks <james@bluebaytravel.co.uk>
2015-03-03 14:08:44 +0000

    Remove the Learn more button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
         <div class="row">
             <div class="col-md-6">
                 <h1>Cachet keeps your <strong>customers</strong>, <strong>developers</strong> &amp; <strong>share-holders</strong> up to date with your products status.</h1>
-                <a class="btn btn-conf btn-green" href="#">Learn More</a>
                 <a class="btn btn-conf btn-clear" href="https://github.com/cachethq/Cachet/archive/master.zip"><i class='ion-social-github'></i> Download from GitHub</a>
             </div>
         </div>
@@ -16,15 +15,15 @@
 <section class="green">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-sm-4">
                 <h4><i class="ion-social-github"></i> Open Source</h4>
                 <p>Cachet is 100% open source, opening a whole world of contributions and new features.</p>
             </div>
-            <div class="col-md-4">
+            <div class="col-sm-4">
                 <h4><i class="ion-heart"></i> Beautiful</h4>
                 <p>From the status page, to the dashboard, right through to the code itself. Cachet is beautifully crafted everywhere.</p>
             </div>
-            <div class="col-md-4">
+            <div class="col-sm-4">
                 <h4><i class="ion-planet"></i> Multilingual</h4>
                 <p>Cachet speaks several languages <strong>and</strong> you can contribute to existing translations or provide new ones from within Cachet itself.</p>
             </div>
