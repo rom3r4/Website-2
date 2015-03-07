025e23a8921d4961ef6e6e7155f2fb7a072bfcd0
James Brooks <james@bluebaytravel.co.uk>
2015-03-07 11:46:43 +0000

    Format the master template. Add GoSquared tracking.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  @include('partials.head')
+    @include('partials.head')
 
-  <body>
-    @include('partials.navbar')
+    <body>
+        @include('partials.navbar')
 
-    @yield('content')
+        @yield('content')
 
-    @include('partials.footer')
+        @include('partials.footer')
 
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/retina-1.1.0.js"></script>
-  </body>
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/retina-1.1.0.js"></script>
+
+        <script>
+            !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+            arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+            d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+            insertBefore(d,q)}(window,document,'script','_gs');
+            _gs('GSN-247567-S');
+        </script>
+    </body>
 </html>
