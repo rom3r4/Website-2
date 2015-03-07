b531e1785aef99c8078eef1c691c2e5b243b029c
James Brooks <james@bluebaytravel.co.uk>
2015-03-07 11:56:20 +0000

    Added download tracking

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,11 +14,11 @@
         <script src="/js/retina-1.1.0.js"></script>
 
         <script>
-            !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
-            arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
-            d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
-            insertBefore(d,q)}(window,document,'script','_gs');
-            _gs('GSN-247567-S');
+        (function ($, window, undefined) {
+            $('a[rel=download]').on('click', function () {
+                _gs('event', 'Download');
+            });
+        }(jQuery, window));
         </script>
     </body>
 </html>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -20,4 +20,12 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script>
+        !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+        arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+        d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+        insertBefore(d,q)}(window,document,'script','_gs');
+        _gs('GSN-247567-S');
+    </script>
 </head>
