5ac23093ad9b15805114e6ef7be475f1fa188bdf
James Brooks <james@bluebaytravel.co.uk>
2015-03-07 13:59:39 +0000

    Social links, footer change.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,14 @@
-<footer>
+<footer class="centered">
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
+                <div class="row mb">
+                    <div class="col-md-12 social-links">
+                        <a href="https://twitter.com/cachethq" target="_blank"><i class="ion ion-social-twitter"></i></a>
+                        <a href="https://facebook.com/cachethq" target="_blank"><i class="ion ion-social-facebook"></i></a>
+                        <a href="https://github.com/cachethq" target="_blank"><i class="ion ion-social-github"></i></a>
+                    </div>
+                </div>
                 <div class="copyright">&copy; Cachet HQ {{ date("Y") }}</div>
             </div>
         </div>
