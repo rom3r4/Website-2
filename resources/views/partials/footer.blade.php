09248fd64b84da1e91502bf7026a727e1f45aad7
James Brooks <james@bluebaytravel.co.uk>
2015-03-10 08:46:32 +0000

    Added support email link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,6 +7,7 @@
                         <a href="https://twitter.com/cachethq" target="_blank"><i class="ion ion-social-twitter"></i></a>
                         <a href="https://facebook.com/cachethq" target="_blank"><i class="ion ion-social-facebook"></i></a>
                         <a href="https://github.com/cachethq" target="_blank"><i class="ion ion-social-github"></i></a>
+                        <a href="mailto:support@cachethq.io"><i class="ion ion-email"></i></a>
                     </div>
                 </div>
                 <div class="copyright">&copy; Cachet HQ {{ date("Y") }}</div>
