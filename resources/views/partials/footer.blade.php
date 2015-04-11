83b5d1477ecf6a7ff6af65ef22325ca569dd7c93
James Brooks <james@bluebaytravel.co.uk>
2015-04-11 07:55:30 +0100

    Improved footer layout.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,24 +1,54 @@
-<footer class="">
-    <div class="container">
-        <div class="row mb">
-            <div class="col-sm-4">
-                <ul class="list-inline">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/team">Team</a></li>
-                    <li><a href="https://docs.cachethq.io">Docs</a></li>
-                    <li><a href="https://blog.cachethq.io">Blog</a></li>
-                </ul>
-            </div>
-            <div class="col-sm-8 social-links centered">
-                <a href="https://twitter.com/cachethq" target="_blank"><i class="ion ion-social-twitter"></i></a>
-                <a href="https://facebook.com/cachethq" target="_blank"><i class="ion ion-social-facebook"></i></a>
-                <a href="https://github.com/cachethq" target="_blank"><i class="ion ion-social-github"></i></a>
-                <a href="mailto:support@cachethq.io"><i class="ion ion-email"></i></a>
+<!-- Footer -->
+<footer id="footer">
+    <div class="wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-3 i">
+                    <h2>Cachet HQ</h2>
+                    <div class="content">
+                        <a href='&#109;ail&#116;o&#58;su%&#55;0%70&#37;&#54;Frt&#37;40ca%63hethq&#46;%69&#111;'>s&#117;ppor&#116;&#64;c&#97;c&#104;eth&#113;&#46;io</a>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-3 i">
+                    <h2>Newsletter</h2>
+                    <form role='form' action="//cachethq.us9.list-manage.com/subscribe/post?u=654f358550ef074d6601475cd&amp;id=711cf8b66d" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+                        <label for="mce-EMAIL" class='sr-only'>Email Address  <span class="asterisk">*</span></label>
+                        <input type="email" value="" required name="EMAIL" class="form-control" id="mce-EMAIL" placeholder="joe@cachet-rocks.io">
+
+                        <button type="submit" class='submit' name="subscribe" id="mc-embedded-subscribe"></button>
+
+                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_654f358550ef074d6601475cd_711cf8b66d" tabindex="-1" value=""></div>
+                    </form>
+                </div>
+                <div class="col-xs-6 col-sm-3 i">
+                    <h2>Follow</h2>
+                    <div class="socials">
+                        <a href="https://facebook.com/CachetHQ" title="Like us on Facebook" class="facebook"><i class="icon ion-social-facebook"></i></a>
+                        <a href="https://twitter.com/cachethq" title="Follow us on Twitter" class="twitter"><i class="icon ion-social-twitter"></i></a>
+                        <a href="https://github.com/cachethq" title="#" class="github"><i class="icon ion-social-github"></i></a>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-3 i">
+                    <h2>Browse</h2>
+                    <div class="content clearfix">
+                        <a href="/">Home</a>
+                        <a href="/team">Team</a>
+                        <a href="https://docs.cachethq.io">Docs</a>
+                        <a href="https://blog.cachethq.io">Blog</a>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row centered">
-            <div class="col-sm-12">
-                <div class="copyright">&copy; Cachet HQ {{ date("Y") }}</div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6">Copyright &copy; {{ date('Y') }} <a href="https://james-brooks.uk/" title="James Brooks">James Brooks</a></div>
+                <div class="col-xs-6 text-right">
+                    Donations: <a href="https://twitter.com/s_mcleod" title="Sam McLeod" target="_blank">Sam McLeod</a>, <a href="https://twitter.com/joecohens" title="Joe Cohen" target="_blank">Joe Cohen</a>
+                </div>
             </div>
         </div>
     </div>
