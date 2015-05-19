7531b7c2daeb9efb2457c6c6731e6201fa227527
James Brooks <james@bluebaytravel.co.uk>
2015-05-19 17:51:22 +0100

    Add the newsletter to the top of the website too

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,6 +39,8 @@
     </div>
 </section>
 
+@include('partials.newsletter', ['condensed' => true])
+
 <section>
     <div class="container">
         <div class="row mtb">
@@ -137,22 +139,5 @@
     </div>
 </section>
 
-<section class="primary-green subscribe">
-    <div class="container">
-        <div class="row centered">
-            <h2>Don't miss anything. Subscribe to be updated.</h2>
-
-            <div class="col-md-6 col-md-offset-3 mts">
-                <form role='form' action="//cachethq.us9.list-manage.com/subscribe/post?u=654f358550ef074d6601475cd&amp;id=711cf8b66d" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
-                    <label for="mce-EMAIL" class='sr-only'>Email Address  <span class="asterisk">*</span></label>
-                    <input type="email" value="" required name="EMAIL" class="subscribe-input" id="mce-EMAIL" placeholder="joe@cachet-rocks.io">
-
-                    <button type="submit" class='btn btn-conf btn-clear' name="subscribe" id="mc-embedded-subscribe">Sign up</button>
-
-                    <div style="position: absolute; left: -5000px;"><input type="text" name="b_654f358550ef074d6601475cd_711cf8b66d" tabindex="-1" value=""></div>
-                </form>
-            </div>
-        </div>
-    </div>
-</section>
+@include('partials.newsletter')
 @endsection
---
+++ resources/views/partials/newsletter.blade.php
@@ -0,0 +1,18 @@
+<section class="primary-green subscribe less">
+    <div class="container">
+        <div class="row centered">
+            <h2>Don't miss anything. Subscribe to be updated.</h2>
+
+            <div class="col-md-6 col-md-offset-3 mts">
+                <form role='form' action="//cachethq.us9.list-manage.com/subscribe/post?u=654f358550ef074d6601475cd&amp;id=711cf8b66d" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+                    <label for="mce-EMAIL" class='sr-only'>Email Address  <span class="asterisk">*</span></label>
+                    <input type="email" value="" required name="EMAIL" class="subscribe-input" id="mce-EMAIL" placeholder="joe@cachet-rocks.io">
+
+                    <button type="submit" class='btn btn-conf btn-clear' name="subscribe" id="mc-embedded-subscribe">Sign up</button>
+
+                    <div style="position: absolute; left: -5000px;"><input type="text" name="b_654f358550ef074d6601475cd_711cf8b66d" tabindex="-1" value=""></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
